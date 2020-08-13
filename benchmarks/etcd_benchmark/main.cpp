d23fb66e6c1cbfad259ece28a35351fafcc567bd
Michael de Lang <kingoipo@gmail.com>
2020-08-13 18:26:13 +0200

    Add etcd benchmark

--- benchmarks/etcd_benchmark/main.cpp
+++ benchmarks/etcd_benchmark/main.cpp
@@ -1,6 +1,6 @@
-#include "TestService.h"
-#include "TestMsgJsonSerializer.h"
+#include "UsingEtcdService.h"
 #include <optional_bundles/logging_bundle/LoggerAdmin.h>
+#include <optional_bundles/etcd_bundle/EtcdService.h>
 #ifdef USE_SPDLOG
 #include <optional_bundles/logging_bundle/SpdlogFrameworkLogger.h>
 #include <optional_bundles/logging_bundle/SpdlogLogger.h>
@@ -14,17 +14,18 @@
 #define FRAMEWORK_LOGGER_TYPE CoutFrameworkLogger
 #define LOGGER_TYPE CoutLogger
 #endif
+#include <string>
 
 int main() {
+    using namespace std::string_literals;
     std::locale::global(std::locale("en_US.UTF-8"));
 
     DependencyManager dm{};
     auto logMgr = dm.createServiceManager<IFrameworkLogger, FRAMEWORK_LOGGER_TYPE>();
     logMgr->setLogLevel(LogLevel::INFO);
     dm.createServiceManager<ILoggerAdmin, LoggerAdmin<LOGGER_TYPE>>();
-    dm.createServiceManager<ISerializationAdmin, SerializationAdmin>(RequiredList<ILogger>, OptionalList<>);
-    dm.createServiceManager<ISerializer, TestMsgJsonSerializer>(RequiredList<ILogger, ISerializationAdmin>, OptionalList<>);
-    dm.createServiceManager<ITestService, TestService>(RequiredList<ILogger, ISerializationAdmin>, OptionalList<>);
+    dm.createServiceManager<IEtcdService, EtcdService>(RequiredList<ILogger>, OptionalList<>, CppelixProperties{{"EtcdAddress", "localhost:2379"s}});
+    dm.createServiceManager<IUsingEtcdService, UsingEtcdService>(RequiredList<ILogger, IEtcdService>, OptionalList<>);
     dm.start();
 
     return 0;
