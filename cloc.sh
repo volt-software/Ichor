7fc2730af67dc99065f9c5775e6b39b0a517d16b
Michael de Lang <kingoipo@gmail.com>
2024-02-19 16:20:42 +0100

    Split out cloc by examples/benchmarks/tests/main

--- cloc.sh
+++ cloc.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cloc src/ include/ test/ examples/ benchmarks/ --exclude-dir=tl,sole,backward,gsm_enc,base64,ctre,ankerl --exclude-content=LYRA_LYRA_HPP --by-file
-echo ""
-cloc src/ include/ test/ examples/ benchmarks/ --exclude-dir=tl,sole,backward,gsm_enc,base64,ctre,ankerl --exclude-content=LYRA_LYRA_HPP
+echo -e "=== by file ==="
+cloc --quiet --hide-rate src/ include/ test/ examples/ benchmarks/ --exclude-dir=tl,sole,backward,gsm_enc,base64,ctre,ankerl --exclude-content=LYRA_LYRA_HPP --by-file
+echo -e "=== main ==="
+cloc --quiet --hide-rate src/ include/ --exclude-dir=tl,sole,backward,gsm_enc,base64,ctre,ankerl --exclude-content=LYRA_LYRA_HPP
+echo -e "=== test ==="
+cloc --quiet --hide-rate test/ --exclude-dir=tl,sole,backward,gsm_enc,base64,ctre,ankerl --exclude-content=LYRA_LYRA_HPP
+echo -e "=== examples ==="
+cloc --quiet --hide-rate examples/ --exclude-dir=tl,sole,backward,gsm_enc,base64,ctre,ankerl --exclude-content=LYRA_LYRA_HPP
+echo -e "=== benchmarks ==="
+cloc --quiet --hide-rate benchmarks/ --exclude-dir=tl,sole,backward,gsm_enc,base64,ctre,ankerl --exclude-content=LYRA_LYRA_HPP
+echo -e "=== total ==="
+cloc --quiet --hide-rate src/ include/ test/ examples/ benchmarks/ --exclude-dir=tl,sole,backward,gsm_enc,base64,ctre,ankerl --exclude-content=LYRA_LYRA_HPP
