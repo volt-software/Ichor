601e2023f829e4a74dc48a4ca6365e439f065e54
Michael de Lang <kingoipo@gmail.com>
2024-06-23 23:06:24 +0200

    WIP more Etcd v3 implementation

--- test/TestServices/Etcdv3UsingService.h
+++ test/TestServices/Etcdv3UsingService.h
@@ -14,6 +14,8 @@ namespace Ichor {
         Task<tl::expected<void, Ichor::StartError>> start() final {
             co_await put_get_delete_test();
             co_await txn_test();
+            co_await leases_test();
+            co_await compact_test();
 
             GetThreadLocalEventQueue().pushEvent<QuitEvent>(getServiceId());
 
@@ -84,7 +86,7 @@ namespace Ichor {
         Task<void> txn_test() const {
             ICHOR_LOG_INFO(_logger, "running test");
             int64_t revision{};
-            Etcd::v3::EtcdPutRequest putReq{"v3_txn_key", "txn_value", 0, tl::nullopt, tl::nullopt, tl::nullopt};
+            Etcd::v3::EtcdPutRequest putReq{"v3_txn_key", "txn_value", tl::nullopt, tl::nullopt, tl::nullopt, tl::nullopt};
 
             auto putReply = co_await _etcd->put(putReq);
             if (!putReply) {
@@ -105,17 +107,107 @@ namespace Ichor {
             Etcd::v3::EtcdTxnRequest txnReq{};
             txnReq.compare.emplace_back(Etcd::v3::EtcdCompare{.target = Etcd::v3::EtcdCompareTarget::CREATE,.key = "v3_txn_key", .create_revision = revision});
             txnReq.success.emplace_back(Etcd::v3::EtcdRequestOp{.request_range = Etcd::v3::EtcdRangeRequest{.key = "v3_txn_key"}});
-            //txnReq.failure.emplace_back(Etcd::v3::EtcdRequestOp{.request_range = Etcd::v3::EtcdRangeRequest{.key = "v3_txn_key"}});
             auto txnReply = co_await _etcd->txn(txnReq);
             if (!txnReply) {
                 throw std::runtime_error("txn");
             }
 
-            // something is broken with txn, probably has to do with the null fields that glaze adds
+            if(!txnReply->succeeded) {
+                throw std::runtime_error("txn succeeded false");
+            }
+
             if(txnReply->responses.size() != 1) {
                 throw std::runtime_error("txn responses size != 1");
             }
         }
+
+        Task<void> leases_test() const {
+            ICHOR_LOG_INFO(_logger, "running test");
+            Etcd::v3::LeaseGrantRequest grantReq{100, 101};
+
+            auto grantReply = co_await _etcd->leaseGrant(grantReq);
+            if (!grantReply) {
+                throw std::runtime_error("grant");
+            }
+
+            Etcd::v3::LeaseKeepAliveRequest keepAliveReq{101};
+            auto keepAliveReply = co_await _etcd->leaseKeepAlive(keepAliveReq);
+            if (!keepAliveReply) {
+                throw std::runtime_error("keepAlive");
+            }
+            if(keepAliveReply->result.id != 101) {
+                throw std::runtime_error("keepAlive id != 101");
+            }
+            if(keepAliveReply->result.ttl_in_seconds <= 90) {
+                throw std::runtime_error("keepAlive ttl_in_seconds <= 90");
+            }
+
+            Etcd::v3::EtcdPutRequest putReq{"v3_lease_key", "lease_value", 101, tl::nullopt, tl::nullopt, tl::nullopt};
+            auto putReply = co_await _etcd->put(putReq);
+            if (!putReply) {
+                throw std::runtime_error("put");
+            }
+
+            Etcd::v3::LeaseTimeToLiveRequest ttlReq{101, true};
+            auto ttlReply = co_await _etcd->leaseTimeToLive(ttlReq);
+            if (!ttlReply) {
+                throw std::runtime_error("ttl");
+            }
+            if(ttlReply->id != 101) {
+                throw std::runtime_error("ttl id != 101");
+            }
+            if(ttlReply->ttl_in_seconds <= 90) {
+                throw std::runtime_error("ttl ttl_in_seconds <= 90");
+            }
+            if(ttlReply->granted_ttl != 100) {
+                throw std::runtime_error("ttl granted_ttl != 100");
+            }
+            if(ttlReply->keys.size() != 1) {
+                throw std::runtime_error("ttl keys.size() != 1");
+            }
+            if(ttlReply->keys[0] != "v3_lease_key") {
+                throw std::runtime_error("ttl keys[0] != v3_lease_key");
+            }
+
+            Etcd::v3::LeaseLeasesRequest leasesReq{};
+            auto leasesReply = co_await _etcd->leaseLeases(leasesReq);
+            if(!leasesReply) {
+                throw std::runtime_error("leases");
+            }
+            if(leasesReply->leases.size() != 1) {
+                throw std::runtime_error("leases leases.size() != 1");
+            }
+            if(leasesReply->leases[0].id != 101) {
+                throw std::runtime_error("leases leases[0].id != 101");
+            }
+
+            Etcd::v3::LeaseRevokeRequest revokeReq{101};
+            auto revokeReply = co_await _etcd->leaseRevoke(revokeReq);
+            if(!revokeReply) {
+                throw std::runtime_error("revoke");
+            }
+        }
+
+        Task<void> compact_test() const {
+            ICHOR_LOG_INFO(_logger, "running test");
+            int64_t revision{};
+            Etcd::v3::EtcdPutRequest putReq{"v3_compaction_key", "compaction_value", tl::nullopt, tl::nullopt, tl::nullopt, tl::nullopt};
+
+            auto putReply = co_await _etcd->put(putReq);
+            if (!putReply) {
+                throw std::runtime_error("put");
+            }
+
+            revision = putReply->header.revision;
+
+            Etcd::v3::EtcdCompactionRequest compactionRequest{revision, tl::nullopt};
+
+            auto compactionReply = co_await _etcd->compact(compactionRequest);
+            if (!compactionReply) {
+                throw std::runtime_error("compactionReply");
+            }
+        }
+
         Etcd::v3::IEtcd *_etcd;
         ILogger *_logger{};
         Version _v{};
