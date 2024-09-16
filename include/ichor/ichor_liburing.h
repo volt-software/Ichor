5761d090bf0605695ef9ac054b1be6984a547c59
Michael de Lang <kingoipo@gmail.com>
2024-09-16 23:06:19 +0200

    Update liburing to support sanitizing before submission

--- include/ichor/ichor_liburing.h
+++ include/ichor/ichor_liburing.h
@@ -7,14 +7,8 @@
 #    pragma GCC diagnostic ignored "-Wshadow"
 #    pragma GCC diagnostic ignored "-Wconversion"
 #    pragma GCC diagnostic ignored "-Wpedantic"
-#    pragma GCC diagnostic ignored "-Wgnu-pointer-arith"
-#    pragma GCC diagnostic ignored "-Wgnu-anonymous-struct"
-#    pragma GCC diagnostic ignored "-Wgnu-statement-expression-from-macro-expansion"
-#    pragma GCC diagnostic ignored "-Wimplicit-int-conversion"
-#    pragma GCC diagnostic ignored "-Wnested-anon-types"
-#    pragma GCC diagnostic ignored "-Wzero-length-array"
+#    pragma GCC diagnostic ignored "-Wpointer-arith"
 #    pragma GCC diagnostic ignored "-Wcast-align"
-#    pragma GCC diagnostic ignored "-Wc99-extensions"
 #endif
 #include "liburing.h"
 #if defined( __GNUC__ )
