f18ee2a70c32f2346a8a921fae315a722a13daef
Michael de Lang <kingoipo@gmail.com>
2024-09-24 22:30:04 +0200

    Expand debug service

--- examples/common/DebugService.h
+++ examples/common/DebugService.h
@@ -19,13 +19,12 @@ private:
     Task<tl::expected<void, Ichor::StartError>> start() final {
         auto &_timer = _timerFactory->createTimer();
         _timer.setCallback([this]() {
-            auto svcs = dm.getAllServices();
-            for(auto &[id, svc] : svcs) {
-                ICHOR_LOG_INFO(_logger, "Svc {}:{} {}", svc->getServiceId(), svc->getServiceName(), svc->getServiceState());
-            }
+            printServices();
         });
-        _timer.setChronoInterval(1s);
+        _timer.setChronoInterval(500ms);
         _timer.startTimer();
+
+        printServices();
 
         co_return {};
     }
@@ -33,6 +32,43 @@ private:
     Task<void> stop() final {
         // the timer factory also stops all timers registered to this service
         co_return;
+    }
+
+    void printServices() {
+        auto svcs = GetThreadLocalManager().getAllServices();
+        for(auto &[id, svc] : svcs) {
+            ICHOR_LOG_INFO(_logger, "Svc {}:{} {}", svc->getServiceId(), svc->getServiceName(), svc->getServiceState());
+
+            ICHOR_LOG_INFO(_logger, "\tInterfaces:");
+            for(auto &iface : GetThreadLocalManager().getProvidedInterfacesForService(svc->getServiceId())) {
+                ICHOR_LOG_INFO(_logger, "\t\tInterface {} hash {}", iface.getInterfaceName(), iface.interfaceNameHash);
+            }
+
+            ICHOR_LOG_INFO(_logger, "\tProperties:");
+            for(auto &[key, val] : svc->getProperties()) {
+                ICHOR_LOG_INFO(_logger, "\t\tProperty {} value {} size {} type {}", key, val, val.get_size(), val.type_name());
+            }
+
+            auto deps = GetThreadLocalManager().getDependencyRequestsForService(svc->getServiceId());
+            ICHOR_LOG_INFO(_logger, "\tDependencies:");
+            for(auto &dep : deps) {
+                ICHOR_LOG_INFO(_logger, "\t\tDependency {} flags {} satisfied {}", dep.getInterfaceName(), dep.flags, dep.satisfied);
+            }
+
+            auto dependants = GetThreadLocalManager().getDependentsForService(svc->getServiceId());
+            ICHOR_LOG_INFO(_logger, "\tUsed by:");
+            for(auto &dep : dependants) {
+                ICHOR_LOG_INFO(_logger, "\t\tDependant {}:{}", dep->getServiceId(), dep->getServiceName());
+            }
+
+            auto trackers = GetThreadLocalManager().getTrackersForService(svc->getServiceId());
+            ICHOR_LOG_INFO(_logger, "\tTrackers:");
+            for(auto &tracker : trackers) {
+                ICHOR_LOG_INFO(_logger, "\t\tTracker for interface {} hash {}", tracker.interfaceName, tracker.interfaceNameHash);
+            }
+
+            ICHOR_LOG_INFO(_logger, "");
+        }
     }
 
     void addDependencyInstance(ILogger &logger, IService &) {
