3fbf733c3bed3d2397029279020021ebcb3049b5
Michael de Lang <kingoipo@gmail.com>
2025-08-12 21:07:30 +0200

    services/openssl: WIP

--- include/ichor/services/network/ISSL.h
+++ include/ichor/services/network/ISSL.h
@@ -1,23 +1,85 @@
 #pragma once
 
 #include <cstdint>
-#include <string_view>
+#include <memory>
 #include <tl/expected.h>
+#include <ichor/stl/StrongTypedef.h>
+
+#include "ichor/stl/NeverAlwaysNull.h"
+#include "tl/optional.h"
 
 namespace Ichor::v1 {
-    struct TLSContext {
-        TLSContext(void *ctx) : _ctx{ctx} {}
-        virtual ~TLSContext() = default;
+    struct TLSCertificateIdType : StrongTypedef<uint64_t, TLSCertificateIdType> {};
+    struct TLSCertificateTypeType : StrongTypedef<uint64_t, TLSCertificateTypeType> {};
+    struct TLSCertificateStoreIdType : StrongTypedef<uint64_t, TLSCertificateStoreIdType> {};
+    struct TLSCertificateStoreTypeType : StrongTypedef<uint64_t, TLSCertificateStoreTypeType> {};
+    struct TLSContextIdType : StrongTypedef<uint64_t, TLSContextIdType> {};
+    struct TLSContextTypeType : StrongTypedef<uint64_t, TLSContextTypeType> {};
+    struct TLSConnectionIdType : StrongTypedef<uint64_t, TLSConnectionIdType> {};
+    struct TLSConnectionTypeType : StrongTypedef<uint64_t, TLSConnectionTypeType> {};
 
-        // virtual
+    struct TLSCertificate {
+        TLSCertificate(NeverNull<void*> ctx, TLSCertificateIdType id) : _ctx{ctx}, _id{id} {}
+        virtual ~TLSCertificate() = default;
+
+#if defined(ICHOR_USE_HARDENING) || defined(ICHOR_ENABLE_INTERNAL_DEBUGGING)
+        [[nodiscard]] virtual TLSCertificateTypeType getType() const noexcept = 0;
+#endif
+
+        [[nodiscard]] virtual std::string_view getCommonName() const noexcept = 0;
+
+    protected:
+        NeverNull<void*> _ctx;
+        TLSCertificateIdType _id{};
+
+    };
+
+    struct TLSCertificateStore {
+        TLSCertificateStore(void *ctx, TLSCertificateStoreIdType id) : _ctx{ctx}, _id{id} {}
+        virtual ~TLSCertificateStore() = default;
+
+#if defined(ICHOR_USE_HARDENING) || defined(ICHOR_ENABLE_INTERNAL_DEBUGGING)
+        [[nodiscard]] virtual TLSCertificateStoreTypeType getType() const noexcept = 0;
+#endif
+
+        [[nodiscard]] virtual tl::optional<std::unique_ptr<TLSCertificate>> getCurrentCertificate() const noexcept = 0;
 
     protected:
         void *_ctx{};
+        TLSCertificateStoreIdType _id{};
+    };
+
+    struct TLSContext {
+        TLSContext(void *ctx, TLSContextIdType id) : _ctx{ctx}, _id{id} {}
+        virtual ~TLSContext() = default;
+
+        [[nodiscard]] TLSContextIdType getId() const {
+            return _id;
+        }
+
+#if defined(ICHOR_USE_HARDENING) || defined(ICHOR_ENABLE_INTERNAL_DEBUGGING)
+        [[nodiscard]] virtual TLSContextTypeType getType() const noexcept = 0;
+#endif
+
+    protected:
+        void *_ctx{};
+        TLSContextIdType _id{};
     };
 
     struct TLSConnection {
+        TLSConnection(void *ctx, TLSConnectionIdType id) : _ctx{ctx}, _id{id} {}
         virtual ~TLSConnection() = default;
 
+        [[nodiscard]] TLSConnectionIdType getId() const {
+            return _id;
+        }
+
+#if defined(ICHOR_USE_HARDENING) || defined(ICHOR_ENABLE_INTERNAL_DEBUGGING)
+        [[nodiscard]] virtual TLSConnectionTypeType getType() const noexcept = 0;
+#endif
+
+        void *_ctx{};
+        TLSConnectionIdType _id{};
     };
 
     enum class TLSContextError {
@@ -25,16 +87,25 @@ namespace Ichor::v1 {
     };
 
     enum class TLSConnectionError {
+        UNKNOWN
+    };
 
+    struct TLSCreateContextOptions {
+        std::vector<uint8_t> trustedCertificates{};
+        bool allowUnknownCertificates{};
+    };
+
+    struct TLSCreateConnectionOptions {
     };
 
     class ISSL {
     public:
 
-        virtual tl::expected<TLSContext, TLSContextError> createTLSContext() = 0;
-        virtual tl::expected<TLSConnection, TLSConnectionError> createTLSConnection(TLSContext&) = 0;
-        virtual tl::expected<void, bool> TLSWrite(TLSContext&, std::string_view) = 0;
-        virtual tl::expected<void, bool> TLSRead(TLSContext&, std::string_view) = 0;
+        [[nodiscard]] virtual tl::expected<std::unique_ptr<TLSContext>, TLSContextError> createServerTLSContext(std::vector<uint8_t> cert, std::vector<uint8_t> key, TLSCreateContextOptions) = 0;
+        [[nodiscard]] virtual tl::expected<std::unique_ptr<TLSContext>, TLSContextError> createClientTLSContext(TLSCreateContextOptions) = 0;
+        [[nodiscard]] virtual tl::expected<std::unique_ptr<TLSConnection>, TLSConnectionError> createTLSConnection(TLSContext&, TLSCreateConnectionOptions) = 0;
+        [[nodiscard]] virtual tl::expected<void, bool> TLSWrite(TLSConnection&, const std::vector<uint8_t> &) = 0;
+        [[nodiscard]] virtual tl::expected<std::vector<uint8_t>, bool> TLSRead(TLSConnection&) = 0;
 
     protected:
         ~ISSL() = default;
